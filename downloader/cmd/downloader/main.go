98482ce4e52d186153e7435943c21f5106bba5b6
hack3r911 <afanasevevgenij2004@gmail.com>
2026-08-17 12:14:09 +0300

    feat: add config handling

--- downloader/cmd/downloader/main.go
+++ downloader/cmd/downloader/main.go
@@ -7,12 +7,6 @@ import (
 	"github.com/HACK3R911/practice/downloader/internal/cli"
 )
 
-var (
-//outputFile = flag.String("out", "downloads/", "Файл для записи результата")
-
-// verbose    = flag.Bool("v", false, "Подробный вывод")
-)
-
 func main() {
 	cfg, err := cli.ParseConfig(os.Args[1:])
 	if err != nil {
@@ -28,20 +22,4 @@ func main() {
 	fmt.Printf("  urls:       %s\n", len(cfg.URLs))
 	fmt.Println()
 	fmt.Println("download is not implemented yet")
-
-	//if len(os.Args) < 3 {
-	//	fmt.Println("Использование: downloader <директория> <url1> [url2...]")
-	//	os.Exit(1)
-	//}
-	//
-	//savePath := os.Args[1]
-	//urls := os.Args[2:]
-	//
-	//fmt.Println("Директория для сохранения:", savePath)
-	//fmt.Println("URL для скачивания:")
-	//for _, url := range urls {
-	//	fmt.Println("-", url)
-	//}
-	//
-	//fmt.Println(os.Args[0])
 }
